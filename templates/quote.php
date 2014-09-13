f5cec2ea71565150d0287e93c2eae326c83fa799
Bryan Deloeste <zipncompress@yahoo.com>
2014-09-12 20:44:51 -0500

    added top menu

--- templates/quote.php
+++ templates/quote.php
@@ -1,3 +1,12 @@
+<div>
+<ul class="nav nav-justified">
+    <li><a href="quote.php">Quote</a></li>
+    <li><a href="buy.php">Buy</a></li>
+    <li><a href="sell.php">Sell</a></li>
+    <li><a href="history.php">History</a></li>
+    <li><a href="logout.php"><strong>Log Out</strong></a></li>
+</ul>
+</div>
 <?php  
        
     $stock = lookup($_POST["symbol"]);
